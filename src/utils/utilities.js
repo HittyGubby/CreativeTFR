ff8cb71cc799ff04952a6a561ce87114fa5174e0
HittyGubby <HitGub@outlook.com>
2025-10-14 02:34:18 +0800

    more sfx

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,8 +1,13 @@
 import { saveData } from "./onload.js";
 import { state } from "./state.js";
+import { Howl } from "howler";
 
 export function Edittext(el) {
   if (el.dataset.editing) return;
+  new Howl({
+    src: ["/sfx/click_province_01.wav"],
+    volume: 1,
+  }).play();
   el.dataset.editing = "true";
   const rawHTML = el.innerHTML;
   const editable = el.cloneNode(true);
@@ -26,6 +31,10 @@ export function Edittext(el) {
     el.style.display = "";
     delete el.dataset.editing;
     editable.remove();
+    new Howl({
+      src: ["/sfx/click_ok.wav"],
+      volume: 1,
+    }).play();
     saveData();
   });
   editable.addEventListener("keydown", (e) => {
